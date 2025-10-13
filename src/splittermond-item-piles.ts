f1fa6ec43d9e86910667848f0db877bd12d7e809
m-2squared <176928581+m-2squared@users.noreply.github.com>
2025-10-13 19:28:51 +0200

    fix(Release): adapt splittermond version
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- src/splittermond-item-piles.ts
+++ src/splittermond-item-piles.ts
@@ -116,6 +116,7 @@ Hooks.once("item-piles-ready", async () => {
         "13.6.2": release_13_config,
         "13.6.3": release_13_config,
         "13.6.4": release_13_config,
+        "13.6.5": release_13_config,
     }
 
     // Add configuration into item piles via the API
